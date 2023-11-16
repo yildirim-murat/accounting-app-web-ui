36ec7f2d4719a0a12a149b64ed2d8f4a03a0e260
yildirim-murat <muratyildirim.work@gmail.com>
2023-11-16 15:01:16 +0300

    Configuration's added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {configureStore} from "./store/configureStore";
+import {saveState} from "./localStorage";
+import {throttle} from "lodash";
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const store = configureStore();
+
+store.subscribe(throttle(() => {
+    saveState({
+        user: store.getState().user
+    });
+}, 1000))
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
